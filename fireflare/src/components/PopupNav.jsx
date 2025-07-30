3142550e78b70ab7428189110a4983d61830cca0
Jeffrey Tsai <jeffrey.my.tsai@gmail.com>
2025-07-29 23:31:39 -0500

    updated reports, added user menu

---
+++ fireflare/src/components/PopupNav.jsx
@@ -0,0 +1,28 @@
+import './components.css'
+import { motion } from 'motion/react';
+
+export const PopupNav = () => {
+  return (
+    <div className="popupNav"
+    >
+        <button>
+            User
+
+        </button>
+        <div>
+            <h2>
+                Test
+            </h2>
+            <button>
+                Settings
+            </button>
+            <button>
+                All Reports
+            </button>
+            <button>
+                Logout
+            </button>
+        </div>  
+    </div>
+  )
+}
